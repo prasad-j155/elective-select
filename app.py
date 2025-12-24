dc0f026e0d91622fd4c4f0121f4f056b293abff2
prasad-j155 <78793296+prasad-j155@users.noreply.github.com>
2025-12-24 19:56:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from google.oauth2 import service_account
 from googleapiclient.discovery import build
 
 # ------------------ CONFIG ------------------ #
-EXCEL_FILE = "allstudents.xlsx"
+EXCEL_FILE = "student-new.xlsx"
 SPREADSHEET_ID = "1y8SlCPHeeUHCi1o3vfjNhEF1bi2fQHtQ4NxHeRT_Blk"  # Replace with your Sheet ID
 SHEET_NAME = "electivedata"  # Or change to your actual sheet name
 
@@ -166,6 +166,7 @@ if st.session_state.sis_verified:
                         st.rerun()
                     except Exception as e:
                         st.error(f"❌ Error writing to Google Sheet: {e}")
+
 
 
 
